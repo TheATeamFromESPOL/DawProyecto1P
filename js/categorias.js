03681f2cbfc53da393ce70ffb6fa9aa0ce4a4869
llullanqui <luis.1142@hotmail.com>
2018-06-18 23:23:13 -0500

    ..

---
+++ js/categorias.js
@@ -0,0 +1,25 @@
+(function(){ })();
+
+var itemsMenu = document.querySelectorAll('a[class*="list-group-item"]');
+var itemsContenido = document.querySelectorAll('div[id^="cont-"]');
+
+function activar(ja){
+	for(i of itemsMenu){
+		if(i == ja){
+			i.classList.add("active");
+		}else{
+			i.classList.remove("active");
+		}
+	}
+
+	for(j of itemsContenido){
+		j.classList.remove("d-flex");
+		j.classList.remove("d-none");
+		if(j.id.includes(ja.id)){
+			j.classList.remove("d-flex");
+		}else{
+			j.classList.add("d-none");
+		}
+	}
+
+}
