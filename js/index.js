fb501776312c51016672945b0d37ffa710ada4bd
llullanqui <luis.1142@hotmail.com>
2018-06-24 20:37:55 -0500

    archivo datos xml corregido:

--- js/index.js
+++ js/index.js
@@ -1,9 +1,10 @@
 (function(){
 
-	//Para cargar quejas
-	$.getJSON("data/datos.json",function(data){
-		var arr = data.quejas
-		console.log(arr);
+	$.ajax({
+		type: "GET",
+		url: "data/quejas.xml",
+		dataType: "xml",
+		success: obtenerQuejas
 	});
 
 })();
@@ -13,4 +14,23 @@ var quejas=$(".encabezadoQueja").find('*').click(cargarNoticia);
 function cargarNoticia(){
     var noticia=this.textContent;
     window.location="noticia.html?noticia="+noticia;
+}
+
+function obtenerQuejas(data){
+	$(data).find('categoria').each(function(){
+		var nombreCategoria = $(this).find('nombreCategoria').text();
+		var quejas = $(this).find('queja');
+		for(i of quejas){
+			var id = $(i).find('id').text();
+			var titulo = $(i).find('titulo').text();
+			var fecha = $(i).find('fecha').text();
+			var contenido = $(i).find('contenido').text();
+			var imagen = $(i).find('imagen').text();
+			var nombreUsuario = $(i).find('nombreUsuario').text();
+			var comentarios = $(i).find('comentarios');
+			for(comentario of comentarios){
+				
+			}
+		}
+	})
 }
