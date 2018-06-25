4c0a7027aebd8d651b211fb2bc11d197e95426c0
llullanqui <luis.1142@hotmail.com>
2018-06-24 19:36:29 -0500

    archivo de quejas xml

--- js/index.js
+++ js/index.js
@@ -1,3 +1,13 @@
+(function(){
+
+	//Para cargar quejas
+	$.getJSON("data/datos.json",function(data){
+		var arr = data.quejas
+		console.log(arr);
+	});
+
+})();
+
 var quejas=$(".encabezadoQueja").find('*').click(cargarNoticia);
 
 function cargarNoticia(){
